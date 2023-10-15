7781cbe9747c844b097230cbc70286ded80bed24
ptgms <62300763+ptgms@users.noreply.github.com>
2023-10-15 02:46:08 +0200

    add minable spawners

--- src/main/java/space/ptgms/util/PlayerController.java
+++ src/main/java/space/ptgms/util/PlayerController.java
@@ -1,13 +1,23 @@
 package space.ptgms.util;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
+import org.bukkit.*;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitScheduler;
+
+import java.util.List;
+import java.util.Objects;
 
 import static org.bukkit.Bukkit.getServer;
 
@@ -48,6 +58,81 @@ public class PlayerController implements Listener {
         if (!event.getPlayer().hasPermission("ptgmsUtils.spawnlimit")) {
             event.setCancelled(true);
             event.getPlayer().sendMessage(ChatColor.BOLD + "" + ChatColor.RED + "You aren't allowed to break any blocks!");
+        }
+    }
+
+    // check if mining spawner and give player spawner if they have permission
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onBlockBreakEvent(BlockBreakEvent event) {
+        if (event.getPlayer().getGameMode() == GameMode.CREATIVE) {
+            return;
+        }
+        if (event.getBlock().getType().toString().contains("SPAWNER")) {
+            if (event.getPlayer().hasPermission("ptgmsUtils.spawner")) {
+                // check if mined with silk touch pickaxe
+                if (!event.getPlayer().getInventory().getItemInMainHand().containsEnchantment(org.bukkit.enchantments.Enchantment.SILK_TOUCH)) {
+                    event.setCancelled(true);
+                    event.getPlayer().sendMessage(ChatColor.BOLD + "" + ChatColor.RED + "You need a silk touch pickaxe to mine spawners!");
+                    return;
+                }
+                BlockState state = event.getBlock().getState();
+
+                if (!(state instanceof CreatureSpawner)) {
+                    return;
+                }
+
+                if (((CreatureSpawner) state).getSpawnedType() == null) {
+                    return;
+                }
+
+                var item = new ItemStack(Material.SPAWNER, 1);
+                var blockMeta = item.getItemMeta();
+                assert blockMeta != null;
+                String mobName = mobIDToName(Objects.requireNonNull((((CreatureSpawner) state).getSpawnedType()).getName()));
+                NamespacedKey key = new NamespacedKey(plugin, "spawner-type");
+                blockMeta.setDisplayName(ChatColor.BOLD + "" + ChatColor.DARK_PURPLE + String.format("%s Spawner", mobName));
+                blockMeta.setLore(List.of(" ", String.format("Place this to spawn %ss", mobName)));
+                blockMeta.getPersistentDataContainer().set(key, PersistentDataType.STRING, Objects.requireNonNull((((CreatureSpawner) state).getSpawnedType()).getName()));
+                blockMeta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
+                item.setItemMeta(blockMeta);
+
+                event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), item);
+
+            }
+        }
+    }
+
+    private String mobIDToName(String mobId) {
+        String[] step1 = mobId.toLowerCase().split("_");
+        for (int i = 0; i < step1.length; i++) {
+            step1[i] = step1[i].substring(0, 1).toUpperCase() + step1[i].substring(1);
+        }
+        return String.join(" ", step1);
+    }
+
+    // when spawner is placed, check if player has permission to place it
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onBlockPlaceEvent(BlockPlaceEvent event) {
+        if (event.getBlock().getType() == Material.SPAWNER) {
+            if (!event.getPlayer().hasPermission("ptgmsUtils.spawner")) {
+                event.setCancelled(true);
+                event.getPlayer().sendMessage(ChatColor.BOLD + "" + ChatColor.RED + "You aren't allowed to place spawners!");
+            }
+            event.getBlock().setType(Material.SPAWNER);
+            BlockState state = event.getBlock().getState();
+            if (!(state instanceof CreatureSpawner)) {
+                return;
+            }
+            var blockMeta = event.getItemInHand().getItemMeta();
+            assert blockMeta != null;
+            // get the mob type from the item using the key
+            NamespacedKey key = new NamespacedKey(plugin, "spawner-type");
+            String mobType = blockMeta.getPersistentDataContainer().get(key, PersistentDataType.STRING);
+            if (mobType == null) {
+                return;
+            }
+            ((CreatureSpawner) state).setSpawnedType(EntityType.valueOf(mobType.toUpperCase()));
+            state.update();
         }
     }
 
